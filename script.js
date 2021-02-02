8246a995bb0efa1beda9fbb8f3fc5fabd37bc072
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-02-02 17:06:47 +0100

    update css and add replay func

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let player_X = document.querySelector("#player_X") ;
 let player_O = document.querySelector("#player_O") ;
 let welcome_section = document.querySelector("#Welcome_section");
 let game_section = document.querySelector("#Game_section");
+let resultat_section = document.querySelector('#resultat_section');
 let player = document.querySelector("#player");
-var CURRENT_PLAYER = "";
+let winner_text = document.querySelector("#winner")
+let replay_button = document.querySelector("#replay");
+
+let CURRENT_PLAYER = "";
 
 
 player_X.addEventListener("click", () =>{
@@ -29,7 +33,9 @@ player_O.addEventListener("click", () =>{
 let allCases = document.querySelectorAll(".case")
 allCases.forEach(cell => {
     cell.addEventListener("click", function(event){
-        event.target.innerText = CURRENT_PLAYER;
+        
+        event.target.innerText= CURRENT_PLAYER;
+        // console.log(target);
         
         if (CURRENT_PLAYER === "X"){
             CURRENT_PLAYER = "O";
@@ -41,40 +47,63 @@ allCases.forEach(cell => {
 
 
     checkWin()
-    }, {once: true});
+    }, {once: false});
     
 });
 
 
 function checkWin() {
-    const values = [...allCases].map(cell => cell.innerText);
+    let values = [...allCases].map(cell => cell.innerText);
     console.log("VALUES", values)
 
     if (values[0] !== "" && values[0] === values[3] && values[0] === values[6]) {
-        alert(`if1 = ${values[0]} wins !`)
+        gotoresultat(values[0])
     }
     else if (values[0] !== "" && values[0] === values[1] && values[0] === values[3]) {
-        alert(`if2 = ${values[0]} wins !`)
+        gotoresultat(values[0])
     }
     else if (values[0] !== "" && values[0] === values[4] && values[0] === values[8]) {
-        alert(`if=3 ${values[0]} wins !`)
+        gotoresultat(values[0])
     }
     else if (values[1] !== "" && values[1] === values[4] && values[1] === values[7]) {
-        alert(`if4 ${values[1]} wins !`)
+        gotoresultat(values[1])
     }
     else if (values[2] !== "" && values[2] === values[5] && values[2] === values[8]) {
-        alert(`if5 ${values[2]} wins !`)
+        gotoresultat(values[2])
     }
     else if (values[2] !== "" && values[2] === values[4] && values[2] === values[6]) {
-        alert(`if6 ${values[2]} wins !`)
+        gotoresultat(values[2])
     }
     else if (values[3] !== "" && values[3] === values[4] && values[3] === values[5]) {
-        alert(`if7 ${values[3]} wins !`)
+        gotoresultat(values[3])
     }
     else if (values[6] !== "" && values[6] === values[7] && values[6] === values[8]) {
-        alert(`if8 ${values[6]} wins !`)
+        gotoresultat(values[6])
     }
     else if(values[0] !== "" && values[1] !== "" && values[2] !== "" && values[3] !== "" && values[4] !== "" && values[5] !== "" && values[6] !== "" && values[7] !== "" && values[8] !== ""){
-        alert('Equality !')
+        gotoresultat("Egalité")
     }
 }
+
+let gotoresultat = (winner) => {
+    game_section.classList.add("display_none");
+    resultat_section.classList.remove("display_none");
+
+    if( winner === "X" || winner === "O"){
+        winner_text.innerText =`Le gagnant est ` +  winner;
+    }
+    else{
+        winner_text.innerText ='Egalité'
+    };
+}
+
+
+let replay = replay_button.addEventListener('click', () => {
+    resultat_section.classList.add("display_none");
+    welcome_section.classList.remove("display_none");
+    values = [];
+    CURRENT_PLAYER = "";
+    allCases.forEach(cell => {
+        cell.innerText = ""
+    });
+});
